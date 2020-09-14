8eef0a77b2793dad0f2363602b82607902f16cf6
Megasteel32 <megasteel32@gmail.com>
2020-09-14 15:34:17 -0500

    fuck you

--- lucaprogram.py
+++ lucaprogram.py
@@ -1,3 +1,9 @@
 from sympy import *
 
-x = Symbol('x')
+x = symbols('x')
+
+solution1 = solve(3*x + 10 < 43, x)
+print(solution1)
+
+solution2 = solve(-1 < (7-x)/3, x) and solve((7-x)/3 <= 4, x)
+print(solution2)
